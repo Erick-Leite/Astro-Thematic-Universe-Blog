a59609c959a78256cacfc4aac5274e9819444d67
erickleite338 <erickleite338@gmail.com>
2025-08-12 11:42:04 +0000

    feat(content.config): Add authorsNames field

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,8 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
+import { authorsData } from "./data/authorsData";
+
+const defaultAuthorName = authorsData[0].name;
 
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -9,6 +12,7 @@ const blog = defineCollection({
     z.object({
       title: z.string(),
       description: z.string(),
+      authorsNames: z.array(z.string()).default([defaultAuthorName]),
       // Transform string to Date object
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
