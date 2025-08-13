77bb7fa8c27f491b1d393666b0297173fbca950f
erickleite338 <erickleite338@gmail.com>
2025-08-13 17:33:30 +0000

    feat(content.config): Add optional heroImageAlt field
    - Set an empty string as the default value of the heroImageAlt field
    - Make the heroImage field mandatory

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,7 +16,8 @@ const blog = defineCollection({
       // Transform string to Date object
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
+      heroImage: image(),
+      heroImageAlt: z.string().default(""),
     }),
 });
 
