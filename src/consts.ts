840836463d5eb412718bcaeb212a4f88cb01521f
erickleite338 <erickleite338@gmail.com>
2025-08-23 17:40:10 +0000

    feat(consts): Update the SITE_TITLE and SITE_DESCRIPTION variables

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Blog Universo Temático";
+export const SITE_DESCRIPTION =
+  "Blog Universo Temático – espaço feito para compartilhar conhecimentos sobre os mais variados temas e, também, para dar voz a outras pessoas que queiram dividir suas ideias por meio de artigos.";
