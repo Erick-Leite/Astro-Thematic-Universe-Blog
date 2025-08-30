0ca923fa2783955b59c0cb545ad5ee22ca092387
erickleite338 <erickleite338@gmail.com>
2025-08-30 17:28:23 +0000

    feat(consts): Add constant variable AUTHOR

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = "Blog Universo Temático";
 export const SITE_DESCRIPTION =
   "Blog Universo Temático – espaço feito para compartilhar conhecimentos sobre os mais variados temas e, também, para dar voz a outras pessoas que queiram dividir suas ideias por meio de artigos.";
+export const AUTHOR = "Pedro Erick";
